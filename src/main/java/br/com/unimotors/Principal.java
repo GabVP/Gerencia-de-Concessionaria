c1bbe1da0e961e8154bf871a115e8fe4eeda0580
Lucas <lucasvp954@gmail.com>
2024-12-01 13:23:14 -0300

    Primeiro commit

---
+++ src/main/java/br/com/unimotors/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.unimotors;
+
+import br.com.unimotors.view.TelaInicial;
+
+
+
+/**
+ *
+ * @author lab5.ccet
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        TelaInicial tela = new TelaInicial();
+        tela.setVisible(true);
+    }
+}
